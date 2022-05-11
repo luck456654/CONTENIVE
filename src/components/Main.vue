7285c6ded04b561e7d9e25c0387ddd842a3b8f46
Vadim <luckjantsev@rambler.ru>
2022-05-11 22:19:10 +0300

    11052022

---
+++ src/components/Main.vue
@@ -0,0 +1,197 @@
+<template>
+  <body>
+    <div class="modalWindow">
+      <div class="modalWindow__form">
+        <div class="modalWindow__title">Choose a city</div>
+        <div class="modalWindow__desk">
+          To find city start typing and pick one from the suggestions
+        </div>
+        <input class="modalWindow__input" placeholder="Search city" />
+        <div class="modalWindow__buttons">
+          <button class="modalWindow__button">CLEAR</button>
+          <div class="modalWindow__add">
+          <button class="modalWindow__button">CANCEL</button>
+          <button class="modalWindow__button">ADD</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h1 class="title">World Weather</h1>
+    <h2 class="desck">Watch weather in your current location</h2>
+    <form class="form">
+      <div class="form__title">Moscow, RU</div>
+      <p class="form__desk">Your current location</p>
+      <ul class="form__ul">
+        <li class="form__li">
+          <div>Weather</div>
+          <div>Clouds</div>
+        </li>
+        <li class="form__li">
+          <div>Temperature</div>
+          <div>'Температура'</div>
+        </li>
+        <li class="form__li">
+          <div>Humidity</div>
+          <div>'Влажность'</div>
+        </li>
+        <li>
+          <div class="form__time">Время</div>
+        </li>
+        <li>
+          <div class="form__button">RELOAD</div>
+        </li>
+      </ul>
+    </form>
+    <button class="button-add">ADD CITY</button>
+    <form class="form formAddCity">
+      <div class="form__title">Moscow, RU</div>
+      <p class="form__desk">Your current location</p>
+      <ul class="form__ul formAddCity__ul">
+        <li class="form__li">
+          <div>Weather</div>
+          <div>Clouds</div>
+        </li>
+        <li class="form__li">
+          <div>Temperature</div>
+          <div>'Температура'</div>
+        </li>
+        <li class="form__li">
+          <div>Humidity</div>
+          <div>'Влажность'</div>
+        </li>
+        <li>
+          <div class="form__time">Время</div>
+        </li>
+        <li class="form__li formAddCity__li">
+          <div class="form__button">REMOVE</div>
+          <div class="form__button">RELOAD</div>
+        </li>
+      </ul>
+    </form>
+    <svg
+      class="button__svd-add"
+      viewBox="0 0 76 76"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_ddd_20_7)">
+        <path
+          d="M10 37C10 21.536 22.536 9 38 9V9C53.464 9 66 21.536 66 37V37C66 52.464 53.464 65 38 65V65C22.536 65 10 52.464 10 37V37Z"
+          fill="#9B51E0"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M45 38H39V44H37V38H31V36H37V30H39V36H45V38Z"
+          fill="white"
+          fill-opacity="0.87"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_ddd_20_7"
+          x="0"
+          y="0"
+          width="76"
+          height="76"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2" />
+          <feGaussianBlur stdDeviation="2" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_20_7"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="1" />
+          <feGaussianBlur stdDeviation="5" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect1_dropShadow_20_7"
+            result="effect2_dropShadow_20_7"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2.5" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.14 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect2_dropShadow_20_7"
+            result="effect3_dropShadow_20_7"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect3_dropShadow_20_7"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  </body>
+</template>
+
+<script>
+import axios from "axios";
+import "@/components/css/style.min.css";
+
+const BASEURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=769acfb2b423d6376361ff6032d02022";
+export default {
+  name: "ProductsList",
+  data() {
+    return {
+      message: "777",
+    };
+  },
+  methods: {
+    loadData() {
+      axios.post(BASEURL).then((response) => {
+        this.message = response;
+      });
+    },
+  },
+  created() {
+    this.loadData();
+  },
+};
+</script>
+
+
+
+
+
+
+
+
+
